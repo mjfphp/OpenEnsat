849d2fc014c8aa5d71ea73ddc94eeaea2e7f8636
Zineb Bouchaala <bouchaalazineb@gmail.com>
2017-12-14 10:43:21 +0000

    finition page index

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,16 @@
 @extends('layouts.app2')
 
+    @section('title')
+        Welcome-OpenEnsat
+    @endsection
+
     @section('header')
         <div class="bg-img" style="background-image: url('img/background1.jpg');">
             <div class="overlay"></div>
         </div>
     @endsection
+
+
 
 @section('w')
     <div class="home-wrapper">
@@ -14,11 +20,21 @@
                 <!-- home content -->
                 <div class="col-md-10 col-md-offset-1">
                     <div class="home-content">
-                        <h1 class="white-text">We Are Creative Agency</h1>
-                        <p class="white-text">Morbi mattis felis at nunc. Duis viverra diam non justo. In nisl. Nullam sit amet magna in magna gravida vehicula. Mauris tincidunt sem sed arcu. Nunc posuere.
-                        </p>
-                        <button class="white-btn">Get Started!</button>
-                        <button class="main-btn">Learn more</button>
+                        <img class="logo logo-middle" src="img/logo.png" alt="logo">
+                        <h1 class="white-text title">OpenEnsat</h1>
+                        <p class="white-text">Nous vous présentons tous les cours dont vous aurez besoin pour devenir compétent dans tous domaine.</br>Notre site vous permettra de maitriser les sujets proposés que vous avez suivis.</p>
+                        @if (Route::has('login'))
+                            <div class="top-right links">
+                                @if (Auth::check())
+                                    <a href="{{ url('/home') }}">Home</a>
+                                @else
+                                    <button class="white-btn"><a href="{{ url('/login') }}" class="a-login">Login</a></button>
+                                    <button class="main-btn"><a href="{{ url('/register') }}" class="a-register">Register</a></button>
+                                @endif
+                            </div>
+                        @endif
+                        <!--<button class="white-btn">Get Started!</button>
+                        <button class="main-btn">Learn more</button>-->
                     </div>
                 </div>
                 <!-- /home content -->
@@ -27,4 +43,3 @@
         </div>
     </div>
 @endsection
-
