a233174d5916c1fdbcf22dfc1d891c4766dda4e6
Zineb Bouchaala <bouchaalazineb@gmail.com>
2017-12-18 18:57:49 +0000

    indexDemiFait

--- resources/views/layouts/app3.blade.php
+++ resources/views/layouts/app3.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.app2')
+
 @section('header')
     <!-- Header -->
     <header>
@@ -26,19 +27,17 @@
 
                 <!--  Main navigation  -->
                 <ul class="main-nav nav navbar-nav navbar-right">
+                    @yield('nav','
                     <li><a href="#home">Home</a></li>
                     <li><a href="#about">About</a></li>
                     <li><a href="#portfolio">Cours</a></li>
-                    <li><a href="#service">Forum</a></li>
+                    <li><a href="#service">Forum</a></li>')
                 </ul>
                 <!-- /Main navigation -->
 
             </div>
         </nav>
         <!-- /Nav -->
-
-
-
     </header>
     <!-- /Header -->
 
